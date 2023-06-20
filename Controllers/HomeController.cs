5cb9d9dbf0cccdbc626b3f204c25bf3e16ae04f5
Saad5400 <sdbtwa@gmail.com>
2023-06-20 18:39:13 +0300

    init

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using CS_Razor_Tutorial.Models;
+
+namespace CS_Razor_Tutorial.Controllers;
+
+public class HomeController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
---
+++ Models/Cat.cs
@@ -0,0 +1,12 @@
+namespace CS_Razor_Tutorial.Models;
+
+class Cat
+{
+    public string Name;
+    public int Age;
+    public Cat(string name, int age)
+    {
+        Name = name;
+        Age = age;
+    }
+}
